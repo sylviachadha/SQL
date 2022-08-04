eb8cf59f7272e6da190e6555feacc79817c4fbf7
Sylvia Chadha <sylviachadha@gmail.com>
2022-08-04 18:47:59 +0700

    union

---
+++ dvdrental/union.sql
@@ -0,0 +1,9 @@
+/* Union */
+/* 1. Union is just pasting results of two
+SELECT statements/ stacking results on top
+of each other.
+2. Syntax-
+SELECT col_name FROM table1
+UNION
+SELECT col_name FROM table2;
+ */
