4c94d47f394fc5b0b2a36817f7099d68a1c513f8
Sylvia Chadha <sylviachadha@gmail.com>
2022-08-04 08:39:35 +0700

    having

--- dvdrental/Ques.sql
+++ dvdrental/Ques.sql
@@ -23,4 +23,36 @@ WHERE rating='R' AND replacement_cost BETWEEN 5 AND 15;
 
 /* Q6 - How many films have word Truman somewhere in title?*/
 SELECT * FROM film
-WHERE title LIKE '%Truman%'
+WHERE title LIKE '%Truman%';
+
+
+/* Q7 - Return the customer IDs of customers who have spent at least $110 with
+the staff member who has an ID of 2. */
+
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id=2
+GROUP BY customer_id
+HAVING SUM(amount) > 110;
+
+
+/* Q8 - How many films begin with the letter J?*/
+
+SELECT * FROM film;
+
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'J%';
+
+
+/* Q9 - What customer has the highest customer ID number whose name starts
+with an 'E' and has an address ID lower than 500?*/
+
+SELECT * FROM customer;
+
+SELECT first_name,last_name FROM customer
+WHERE first_name LIKE 'E%' AND address_id<500
+ORDER BY customer_id DESC
+LIMIT 1;
+
+
