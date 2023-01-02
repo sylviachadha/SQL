88ad054f51fd7376361b8e14c8689066a40d0b70
Sylvia Chadha <sylviachadha@gmail.com>
2023-01-02 19:06:58 +0700

    create

--- dvdrental/create.sql
+++ dvdrental/create.sql
@@ -23,6 +23,7 @@ CREATE TABLE job(
 
 
 CREATE TABLE account_job(
+ /*Here  user_id &  job_id are foreign keys*/
     user_id INTEGER REFERENCES user_account(user_id),
     job_id INTEGER REFERENCES job(job_id),
     hire_date TIMESTAMP
