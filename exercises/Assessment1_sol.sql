df6d72fb8b777297bd2eb06c22d87cd3d981e960
Sylvia Chadha <sylviachadha@gmail.com>
2023-02-15 17:57:14 +0700

    assesment1

---
+++ exercises/Assessment1_sol.sql
@@ -0,0 +1,83 @@
+/* Q1 - How many payment txn's were greater than 5$ ?*/
+SELECT COUNT(amount) FROM payment
+WHERE amount > 5;
+
+
+/* Q2 - How many actors have first name that starts with letter P?*/
+SELECT COUNT(*) FROM actor
+WHERE first_name LIKE 'P%';
+
+
+/* Q3 - How many unique districts are our customers from?*/
+SELECT COUNT (DISTINCT(district)) FROM address;
+
+
+/* Q4 -  Retrieve list of names for distinct districts*/
+SELECT DISTINCT(district) FROM address;
+
+
+/* Q5 - How many films have rating 'R' & replacement cost b/w $5 and $15?*/
+SELECT COUNT(*) FROM film
+WHERE rating='R' AND replacement_cost BETWEEN 5 AND 15;
+
+
+/* Q6 - How many films have word Truman somewhere in title?*/
+SELECT * FROM film
+WHERE title LIKE '%Truman%';
+
+
+/* Q7 - Return the customer IDs of customers who have spent at least $110 with
+the staff member who has an ID of 2. */
+
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id=2
+GROUP BY customer_id
+HAVING SUM(amount) > 110;
+
+
+/* Q8 - How many films begin with the letter J?*/
+
+SELECT * FROM film;
+
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'J%';
+
+
+/* Q9 - What customer has the highest customer ID number whose name starts
+with an 'E' and has an address ID lower than 500?*/
+
+SELECT * FROM customer;
+
+SELECT first_name,last_name FROM customer
+WHERE first_name LIKE 'E%' AND address_id<500
+ORDER BY customer_id DESC
+LIMIT 1;
+
+
+/* Q10- What are emails of customers who live in California? */
+
+SELECT * FROM customer;
+SELECT * FROM address;
+
+SELECT email,district
+FROM customer
+INNER JOIN address
+ON customer.address_id = address.address_id
+WHERE district='California';
+
+
+/* Q11 - Get a list of all movies 'Nick Wahlberg' has been in ? */
+
+SELECT * FROM film;
+SELECT * FROM actor;
+SELECT * FROM film_actor;
+
+SELECT title,first_name, last_name
+FROM film_actor
+INNER JOIN film
+ON film.film_id = film_actor.film_id
+INNER JOIN actor
+ON actor.actor_id = film_actor.actor_id
+WHERE first_name = 'Nick' AND last_name = 'Wahlberg';
